e3eb0abef931704cd530df4e16b1aacc8e51abec
Bill-Cipher46 <heidelucas9@gmail.com>
2025-05-14 19:54:31 -0500

    first commit

---
+++ localization/en-us.lua
@@ -0,0 +1,36 @@
+return {
+    descriptions = {
+        Joker = {
+            j_willatro_thecoolerjoker = {
+                name = "The Cooler Joker",
+                text = 
+                {
+                    "{C:white,X:mult}X#1#{} Mult"
+                }
+            },
+
+            j_willatro_loss = {
+                name = "Loss.jpg",
+                text = 
+                {
+                    "If played and scoring hand",
+                    "contains {C:attention}one Queen{} and {C:attention}one 2{},",
+                    "Destroy the {C:attention}2{} and create",
+                    "an {C:attention,T:#info_queue}Ethereal Tag"
+                }
+            },
+
+            j_willatro_jokeinthebox = {
+                name = "Joke In the Box",
+                text = 
+                {
+                    "Gains {C:white,X:mult}X#2#{} Mult for each",
+                    "played hand, resets if played",
+                    "hand contains card of {V:1}#3#{} suit",
+                    "{s:0.8,C:inactive}(Currently {s:0.8,C:white,X:mult}X#1#{} {s:0.8,C:inactive}Mult)",
+                    "{s:0.8,C;inactive}suit changes at end of round"
+                }
+            }
+        }
+    }
+}
