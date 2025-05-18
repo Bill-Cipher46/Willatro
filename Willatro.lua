6fbd53f371548800c11febcf182ea02d104a8ff5
Bill-Cipher46 <heidelucas9@gmail.com>
2025-05-18 09:34:09 -0500

    Add files and references for enhancements and consumables

--- Willatro.lua
+++ Willatro.lua
@@ -1,2 +1,4 @@
 assert(SMODS.load_file("src/jokers.lua"))()
 assert(SMODS.load_file("src/blinds.lua"))()
+assert(SMODS.load_file("src/enhancements.lua"))()
+assert(SMODS.load_file("src/consumables.lua"))()
