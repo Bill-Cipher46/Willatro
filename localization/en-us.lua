b5a1a52467ca99a32f3bb09e8c84f336a1e7339a
Bill-Cipher46 <heidelucas9@gmail.com>
2025-05-18 10:38:51 -0500

    Add localization for jokers

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -56,6 +56,78 @@ return {
                     "{s:0.8,C:inactive}(Currently {s:0.8,C:white,X:mult}X#1#{} {s:0.8,C:inactive}Mult)",
                     "{s:0.8,C;inactive}suit changes at end of round"
                 }
+            },
+
+            j_willatro_familiarweapon = {
+                name = "Familiar Weapon",
+                text = {
+                    "Destroy all played and scoring",
+                    "cards with a rank of {C:attention}8{} or lower.",
+                    "Gain {C:chips}+#1#{} Chips for each",
+                    "destroyed card"
+                }
+            },
+
+            j_willatro_barcode = {
+                name = "Barcode",
+                text = {
+                    "Earn {C:money}$#1#{} at the end of the",
+                    "round for each owned joker",
+                    "{s:0.8,C:inactive}(Currently {C:money,s:0.8}$#2#{s:0.8,C:inactive})"
+                }
+            },
+
+            j_willatro_jokerobot = {
+                name = "Jokerobot",
+                text = {
+                    "All played {C:attention}face{} cards",
+                    "become {C:attention,T:info_queue}Steel{} cards",
+                    "when scored"
+                }
+            },
+
+            j_willatro_rift = {
+                name = "Rift",
+                text = {
+                    "Level up all {C:attention}poker hands{} not",
+                    "played this ante by {C:attention}#1#{} at",
+                    "the end of the ante"
+                }
+            },
+
+            j_willatro_bodyguard = {
+                name = "Bodyguard",
+                text = {
+                    "Protects {C:attention}Joker{} to the right",
+                    "from being sold or destroyed {C:attention}once",
+                    "{C:inactive,s:0.8}(Gets destroyed in place of joker it's protecting,",
+                    "{C:inactive,s:0.8}gets sold for the same value as the joker it's protecting instead)"
+                }
+            },
+
+            j_willatro_jokergummy = {
+                name = "Joker Gummy",
+                text = {
+                    "Gains {C:mult}+#1#{} Mult for each unique",
+                    "{C:attention}Joker{} owned this run",
+                    "{C:inactive,s:0.8}(Currently {s:0.8,C:mult}+#2#{C:inactive,s:0.8} Mult)"
+                }
+            },
+
+            j_willatro_cyan = {
+                name = "Cyan",
+                text = {
+                    "{C:chips}+#1#{} Chips, {C:mult}+#2#{} Mult",
+                    "{C:green}#5# in #6#{} chance for",
+                    "{C:chips}+#3#{} Chips {C:white,X:mult}X#4#{} Mult"
+                }
+            },
+
+            j_willatro_mitosis = {
+                name = "Mitosis",
+                text = {
+
+                }
             }
         },
 
