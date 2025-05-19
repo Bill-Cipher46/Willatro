b6e37d596685051a8dfd591f7c9302dbaed912d1
Bill-Cipher46 <heidelucas9@gmail.com>
2025-05-18 21:53:26 -0500

    Finish enhancement

--- src/enhancements.lua
+++ src/enhancements.lua
@@ -10,7 +10,27 @@ SMODS.Enhancement {
     atlas = "WillatroEnhancements",
     pos = { x = 0, y = 0 },
     config = { extra = { mult = 2 } },
+    
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.mult } }
+    end,
+
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.main_scoring then
+            local overgrown = 0
+            for k, v in ipairs(context.full_hand) do
+                if SMODS.has_enhancement(v, "m_willatro_overgrown") then
+                    overgrown = overgrown + 2
+                end
+            end
+            for k, v in ipairs(G.hand.cards) do
+                if SMODS.has_enhancement(v, "m_willatro_overgrown") then
+                    overgrown = overgrown + 2
+                end
+            end
+            return {
+                mult = overgrown
+            }
+        end
     end
 }
