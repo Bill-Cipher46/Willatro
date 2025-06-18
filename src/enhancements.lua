a587121a9970f762307f12eae9aa18cd5bc1598e
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-17 23:28:46 -0500

    Update enhancement formatting

--- src/enhancements.lua
+++ src/enhancements.lua
@@ -10,10 +10,18 @@ SMODS.Enhancement {
     key = "overgrown",
     atlas = "WillatroEnhancements",
     pos = { x = 0, y = 0 },
-    config = { extra = { mult = 2 } },
+    config = { 
+        extra = { 
+            mult = 2 
+        } 
+    },
     
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
+        return { 
+            vars = { 
+                card.ability.extra.mult 
+            } 
+        }
     end,
 
     calculate = function(self, card, context)
