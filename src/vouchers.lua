0659071d51889ce63df40d5d6bf59b6787c8ed7a
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-18 22:46:13 -0500

    Finish casino regular

--- src/vouchers.lua
+++ src/vouchers.lua
@@ -15,20 +15,48 @@ SMODS.Voucher
     
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = G.P_CENTERS.e_negative
+        info_queue[#info_queue + 1] = G.P_CENTERS.c_wheel_of_fortune
     end
 
 }
 
---casino regular
+--casino regular - done!
 SMODS.Voucher
 {
     key = "regular",
     pos = { x = 1, y = 0 },
     atlas = "WillatroVouchers",
+    config = {
+        extra = {
+            wheels = 2
+        }
+    },
     
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = G.P_CENTERS.e_negative
-        info_queue[#info_queue + 1] = G.P_CENTERS.e_negative
+        info_queue[#info_queue + 1] = G.P_CENTERS.c_wheel_of_fortune
+        return {
+            vars = {
+                card.ability.extra.wheels
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if context.end_of_round and context.game_over == false and context.main_eval and G.GAME.blind.boss then
+            for i = 1, card.ability.extra.wheels do
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        SMODS.add_card {
+                            set = 'Tarot',
+                            key = 'c_wheel_of_fortune',
+                            edition = 'e_negative'
+                        }
+                        return true
+                    end
+                }))
+            end
+        end
     end
 
 }
