8de30040d96208d38bdb41102ba4bf6e1d113de1
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-19 20:48:07 -0500

    Add organ basics

---
+++ src/organs.lua
@@ -0,0 +1,13 @@
+SMODS.Atlas {
+    key = "WillatroOrgans",
+    path = "WillowOrgans.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.rarity {
+    key = "organ",
+    loc_txt = {},
+    badge_colour = HEX("6f222c")
+
+}
