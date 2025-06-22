2437e65d11472eb355d8620e4da839c5b6f3f0cd
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-22 00:30:20 -0500

    Update localization

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -350,10 +350,10 @@ return {
                 name = "Lungs",
                 text = {
                     "Gains {C:chips}+#1#{} chips if played hand",
-                    "contains a {C:attention}pair{}. Resets after {C:attention}#2#{} rounds.",
+                    "contains a {C:attention}Pair{}. Resets after {C:attention}#2#{} rounds.",
                     "Countdown is reset if played hand",
-                    "does not contain a {C:attention}pair",
-                    "{C:inactive}(Currently {C:attention}#3#{C:inactive} rounds remain and {C:chips}+#4#{} Chips)"
+                    "does not contain a {C:attention}Pair",
+                    "{C:inactive}(Currently {C:attention}#3#{C:inactive} rounds remain and {C:chips}+#4#{C:inactive} Chips)"
                 }
             },
             
