b4f36756cf5fdb12e59e0c2ad22baf14104d11bf
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-27 22:29:18 -0500

    Fix overgrown

--- src/enhancements.lua
+++ src/enhancements.lua
@@ -29,7 +29,7 @@ SMODS.Enhancement {
             local overgrown = 0
             for k, v in ipairs(context.full_hand) do
                 if SMODS.has_enhancement(v, "m_willatro_overgrown") then
-                    overgrown = overgrown + card.abiliy.extra.mult
+                    overgrown = overgrown + card.ability.extra.mult
                 end
             end
             for k, v in ipairs(G.hand.cards) do
