e9dcad24bb307332418261049169fdf5d7dfd34f
Bill-Cipher46 <heidelucas9@gmail.com>
2025-06-27 23:27:22 -0500

    Fix heart sprite

--- src/organs.lua
+++ src/organs.lua
@@ -55,18 +55,20 @@ SMODS.Joker {
     end,
 
     draw = function(self, card, layer)
-        if (layer == 'card' or layer == 'both') and card.sprite_facing == 'front' then
-            local scale_mod = 0.05 + 0.05 * math.sin(1.8 * G.TIMERS.REAL) +
-                0.07 * math.sin((G.TIMERS.REAL - math.floor(G.TIMERS.REAL)) * math.pi * 14) *
-                (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 3
-            local rotate_mod = 0.1 * math.sin(1.219 * G.TIMERS.REAL) +
-                0.07 * math.sin((G.TIMERS.REAL) * math.pi * 5) * (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 2
+        if card.config.center.discovered then
+            if (layer == 'card' or layer == 'both') and card.sprite_facing == 'front' then
+                local scale_mod = 0.05 + 0.05 * math.sin(1.8 * G.TIMERS.REAL) +
+                    0.07 * math.sin((G.TIMERS.REAL - math.floor(G.TIMERS.REAL)) * math.pi * 14) *
+                    (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 3
+                local rotate_mod = 0.1 * math.sin(1.219 * G.TIMERS.REAL) +
+                    0.07 * math.sin((G.TIMERS.REAL) * math.pi * 5) * (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 2
 
-            local image = Sprite(0, 0, 71, 95, G.ASSET_ATLAS["willatro_WillatroOrgans"], {x = 1, y = 0})
-            image.role.draw_major = card
-            image:draw_shader('dissolve', 0, nil, nil, card.children.center, scale_mod, rotate_mod, nil,
-                0.1 + 0.03 * math.sin(1.8 * G.TIMERS.REAL), nil, 0.6)
-            image:draw_shader('dissolve', nil, nil, nil, card.children.center, scale_mod, rotate_mod)
+                local image = Sprite(0, 0, 71, 95, G.ASSET_ATLAS["willatro_WillatroOrgans"], {x = 1, y = 0})
+                image.role.draw_major = card
+                image:draw_shader('dissolve', 0, nil, nil, card.children.center, scale_mod, rotate_mod, nil,
+                    0.1 + 0.03 * math.sin(1.8 * G.TIMERS.REAL), nil, 0.6)
+                image:draw_shader('dissolve', nil, nil, nil, card.children.center, scale_mod, rotate_mod)
+            end
         end
     end
 
