d60daa0438eeeeb4af5f172a7e0ed891d98587ef
Bill-Cipher46 <heidelucas9@gmail.com>
2025-07-01 19:48:47 -0500

    Fix heart animation compatability with editions

--- src/organs.lua
+++ src/organs.lua
@@ -74,6 +74,28 @@ SMODS.Joker {
 
 }
 
+SMODS.DrawStep {
+    key = 'willatro_heart',
+    order = 50,
+    func = function(card)
+        if card.config.center.key == "j_willatro_heart" and (card.config.center.discovered or card.bypass_discovery_center) then
+            local scale_mod = 0.05 + 0.05 * math.sin(1.8 * G.TIMERS.REAL) +
+                0.07 * math.sin((G.TIMERS.REAL - math.floor(G.TIMERS.REAL)) * math.pi * 14) *
+                (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 3
+            local rotate_mod = 0.1 * math.sin(1.219 * G.TIMERS.REAL) +
+                0.07 * math.sin((G.TIMERS.REAL) * math.pi * 5) * (1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL))) ^ 2
+
+            local image = Sprite(0, 0, 71, 95, G.ASSET_ATLAS["willatro_WillatroOrgans"], {x = 1, y = 0})
+            image.role.draw_major = card
+            image:draw_shader('dissolve', 0, nil, nil, card.children.center, scale_mod, rotate_mod, nil,
+                0.1 + 0.03 * math.sin(1.8 * G.TIMERS.REAL), nil, 0.6)
+            image:draw_shader('dissolve', nil, nil, nil, card.children.center, scale_mod, rotate_mod)
+        end
+    end,
+    conditions = { vortex = false, facing = 'front' },
+
+}
+
 --brain - done!
 SMODS.Joker {
     key = "brain",
