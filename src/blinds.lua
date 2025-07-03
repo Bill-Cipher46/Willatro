6baa4d7e561731ed1cedf00e7f4b98ffc0522a56
Bill-Cipher46 <heidelucas9@gmail.com>
2025-07-03 16:26:38 -0500

    Fix blinds

--- src/blinds.lua
+++ src/blinds.lua
@@ -20,13 +20,15 @@ SMODS.Blind
     },
     boss_colour = HEX('d09552'),
 
-    calculate = function(self, card, context)
-        if context.discard then
-            for k, v in ipairs(G.hand.cards) do
-                if v.facing == "front" then
-                    v:flip()
+    calculate = function(self, blind, context)
+        if not blind.disabled then
+            if context.discard then
+                for k, v in ipairs(G.hand.cards) do
+                    if v.facing == "front" then
+                        v:flip()
+                    end
                 end
-		    end
+            end
         end
     end
 
@@ -39,16 +41,18 @@ SMODS.Blind
     atlas = "WillatroBlinds",
     pos = { x = 0, y = 1},
     boss = {
-        min = 5,
+        min = 0,
         max = 0
     },
     boss_colour = HEX('9329b7'),
 
     debuff_hand = function(self, cards, hand, handname, check)
-        if handname == 'Pair' or handname == 'High Card' or handname == 'Three of a Kind' or handname == 'Four of a Kind' or 
-        handname == 'Five of a Kind' or handname == 'Flush' or handname == 'Flush Five' or handname == 'Straight Flush' or 
-        handname == 'Flush House' or handname == 'Royal Flush' then
-            return true
+        if not self.disabled then
+            if handname == 'Pair' or handname == 'High Card' or handname == 'Three of a Kind' or handname == 'Four of a Kind' or 
+            handname == 'Five of a Kind' or handname == 'Flush' or handname == 'Flush Five' or handname == 'Straight Flush' or 
+            handname == 'Flush House' or handname == 'Royal Flush' then
+                return true
+            end
         end
     end
 }
@@ -87,13 +91,15 @@ SMODS.Blind
         }
     end,
 
-    calculate = function(self, card, context)
-        if pseudorandom('arrow') < G.GAME.probabilities.normal / self.config.extra.odds then
-            if context.destroy_card and context.cardarea == G.play then
-                return
-                { 
-                    remove = true
-                }
+    calculate = function(self, blind, context)
+        if not blind.disabled then
+            if pseudorandom('arrow') < G.GAME.probabilities.normal / self.config.extra.odds then
+                if context.destroy_card and context.cardarea == G.play then
+                    return
+                    { 
+                        remove = true
+                    }
+                end
             end
         end
     end
