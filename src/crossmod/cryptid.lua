9b84a23a717bbc0f7c7e7d40936af466d7acd926
Bill-Cipher46 <heidelucas9@gmail.com>
2025-09-14 12:21:18 -0500

    add upgrade compat

---
+++ src/crossmod/cryptid.lua
@@ -0,0 +1,55 @@
+SMODS.Atlas
+{
+    key = "WillatroCryptid",
+    path = "WillowCryptid.png",
+    px = 71,
+    py = 95
+}
+
+willatro.upgrades["j_willatro_thecoolerjoker"] = {
+    key = "j_willatro_coolest_joker",
+    upgradeable = true
+}
+
+--the coolest joker
+SMODS.Joker {
+    key = "coolest_joker",
+    rarity = "cry_exotic",
+    atlas = "WillatroCryptid",
+    pos = {x = 0, y = 0 },
+    soul_pos = { x = 1, y = 0 },
+    cost = 50,
+    blueprint_compat = true,
+    pools = {
+        ["willatro_Upgradeable"] = true
+    },
+    config = {
+        extra = {
+            emult = 4
+        }
+    },
+
+    loc_vars = function(self, info_queue, center)
+        return {
+            vars = {
+                number_format(center.ability.extra.emult)
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if context.joker_main and (to_big(card.ability.extra.emult) > to_big(1)) then
+			return {
+				message = localize({
+					type = "variable",
+					key = "a_powmult",
+					vars = {
+						number_format(card.ability.extra.emult),
+					},
+				}),
+				Emult_mod = lenient_bignum(card.ability.extra.emult),
+				colour = G.C.DARK_EDITION,
+			}
+		end
+    end
+}
