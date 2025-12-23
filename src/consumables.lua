e12365d1ef901e2ea8e175e6d6390fd54591b760
Liam Lorentzen <lorentzenliam1@gmail.com>
2025-12-23 16:29:12 -0600

    Update consumables.lua
    potentially fixed the crash when bomb card is used when no jokers are present.

--- src/consumables.lua
+++ src/consumables.lua
@@ -203,8 +203,9 @@ SMODS.Consumable
     end,
 
     use = function(self, card, area, copier)
-        local debuffed_joker = pseudorandom('bomb', 1, #G.jokers.cards)
-        SMODS.debuff_card(G.jokers.cards[debuffed_joker], true, 'bomb')
+        if #G.jokers.cards != nil then
+            local debuffed_joker = pseudorandom('bomb', 1, #G.jokers.cards)
+            SMODS.debuff_card(G.jokers.cards[debuffed_joker], true, 'bomb')
         delay(0.5)
         ease_dollars(card.ability.extra.money)
         delay(0.3)
