49a80d0c6a290cdcdc2b567012639cdda0bcddf8
Liam Lorentzen <lorentzenliam1@gmail.com>
2025-12-23 17:41:08 -0600

    trying to get bodygaurd to work

--- src/jokers.lua
+++ src/jokers.lua
@@ -593,7 +593,7 @@ SMODS.Joker
         if G.jokers and not context.blueprint then
             for i = 1, #G.jokers.cards do
                 if context.selling_self and G.jokers.cards[i+1] == card then
-                    G.jokers.cards[i+1]:set_eternal(true)
+                    G.jokers.cards[i]:set_eternal(true)
                 end
             end
         end
