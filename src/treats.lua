d7af1a08224b2e1deb9cc199a49de9d139c14898
Bill-Cipher46 <heidelucas9@gmail.com>
2026-04-25 17:31:30 -0500

    Finish treat boosters

--- src/treats.lua
+++ src/treats.lua
@@ -9,7 +9,8 @@ SMODS.ConsumableType {
     key = "willatro_treat",
     primary_colour = HEX("282322"),
     secondary_colour = HEX("282322"),
-    collection_rows = { 3, 4 }
+    collection_rows = { 3, 4 },
+    default = "c_willatro_candy"
 }
 
 --candy - done!
@@ -155,4 +156,168 @@ SMODS.Consumable {
         return true
     end
 
+}
+
+--normal booster 1 - done!
+SMODS.Booster {
+    atlas = "WillatroTreats",
+    key = "treat_normal_1",
+    weight = 1,
+    kind = 'willatro_treat',
+    cost = 4,
+    pos = { x = 4, y = 1 },
+    config = { extra = 3, choose = 1 },
+    draw_hand = true,
+
+    loc_vars = function(self, info_queue, card)
+        local cfg = (card and card.ability) or self.config
+        return {
+            vars = {
+                colours = {
+                    HEX("282322")
+                },
+                cfg.choose,
+                cfg.extra
+            },
+            key = self.key:sub(1, -3)
+        }
+    end,
+
+    ease_background_colour = function(self)
+        ease_colour(G.C.DYN_UI.MAIN, HEX("615b5a"))
+        ease_background_colour({ new_colour = HEX("282322"), special_colour = HEX("615b5a"), contrast = 2 })
+    end,
+
+    create_card = function(self, card, i)
+        return {
+            set = "willatro_treat",
+            area = G.pack_cards,
+            skip_materialize = true,
+            key_append =
+            "willatro_treat"
+        }
+    end
+}
+
+--normal booster 2 - done!
+SMODS.Booster {
+    atlas = "WillatroTreats",
+    key = "treat_normal_2",
+    weight = 1,
+    kind = 'willatro_treat',
+    cost = 4,
+    pos = { x = 5, y = 1 },
+    config = { extra = 3, choose = 1 },
+    draw_hand = true,
+
+    loc_vars = function(self, info_queue, card)
+        local cfg = (card and card.ability) or self.config
+        return {
+            vars = {
+                colours = {
+                    HEX("282322")
+                },
+                cfg.choose,
+                cfg.extra
+            },
+            key = self.key:sub(1, -3)
+        }
+    end,
+
+    ease_background_colour = function(self)
+        ease_colour(G.C.DYN_UI.MAIN, HEX("615b5a"))
+        ease_background_colour({ new_colour = HEX("282322"), special_colour = HEX("615b5a"), contrast = 2 })
+    end,
+
+    create_card = function(self, card, i)
+        return {
+            set = "willatro_treat",
+            area = G.pack_cards,
+            skip_materialize = true,
+            key_append =
+            "willatro_treat"
+        }
+    end
+}
+
+--jumbo booster 1 - done!
+SMODS.Booster {
+    atlas = "WillatroTreats",
+    key = "treat_jumbo_1",
+    weight = 1,
+    kind = 'willatro_treat',
+    cost = 6,
+    pos = { x = 0, y = 2 },
+    config = { extra = 5, choose = 1 },
+    draw_hand = true,
+
+    loc_vars = function(self, info_queue, card)
+        local cfg = (card and card.ability) or self.config
+        return {
+            vars = {
+                colours = {
+                    HEX("282322")
+                },
+                cfg.choose,
+                cfg.extra
+            },
+            key = self.key:sub(1, -3)
+        }
+    end,
+
+    ease_background_colour = function(self)
+        ease_colour(G.C.DYN_UI.MAIN, HEX("615b5a"))
+        ease_background_colour({ new_colour = HEX("282322"), special_colour = HEX("615b5a"), contrast = 2 })
+    end,
+
+    create_card = function(self, card, i)
+        return {
+            set = "willatro_treat",
+            area = G.pack_cards,
+            skip_materialize = true,
+            key_append =
+            "willatro_treat"
+        }
+    end
+}
+
+--jumbo booster 2 - done!
+SMODS.Booster {
+    atlas = "WillatroTreats",
+    key = "treat_jumbo_2",
+    weight = 1,
+    kind = 'willatro_treat',
+    cost = 6,
+    pos = { x = 1, y = 2 },
+    config = { extra = 5, choose = 1 },
+    draw_hand = true,
+
+    loc_vars = function(self, info_queue, card)
+        local cfg = (card and card.ability) or self.config
+        return {
+            vars = {
+                colours = {
+                    HEX("282322")
+                },
+                cfg.choose,
+                cfg.extra
+            },
+            key = self.key:sub(1, -3)
+        }
+    end,
+
+    ease_background_colour = function(self)
+        ease_colour(G.C.DYN_UI.MAIN, HEX("615b5a"))
+        ease_background_colour({ new_colour = HEX("282322"), special_colour = HEX("615b5a"), contrast = 2 })
+    end,
+
+    create_card = function(self, card, i)
+        return {
+            set = "willatro_treat",
+            area = G.pack_cards,
+            skip_materialize = true,
+            key_append =
+            "willatro_treat"
+        }
+    end
 }
